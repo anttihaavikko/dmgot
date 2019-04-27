3639393d4e066b902c5a1cbdc83dac6e86a2f564
Antti Haavikko <antti.haavikko@focus.fi>
2019-04-27 12:25:22 +0300

    Add character

---
+++ Assets/Scripts/Farmer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Farmer : MonoBehaviour
+{
+
+    private float moveDelay = 0f;
+    private Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoMove();
+    }
+
+    private void DoMove()
+    {
+        var x = 0f;
+        var y = 0f;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) x = -1f;
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) x = 1f;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) y = -1f;
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) y = 1f;
+
+        var dx = Mathf.Abs(x) > 0.2f ? Mathf.Sign(x) * 1f : 0;
+        var dy = Mathf.Abs(y) > 0.2f ? Mathf.Sign(y) * 1f : 0;
+
+        pos += new Vector3(dx, dy * 0.75f, 0);
+
+        Tweener.Instance.MoveTo(transform, pos, 0.07f, 0f, TweenEasings.BounceEaseOut);
+    }
+}
---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    public GameObject tile;
+
+    public int gridSize = 10;
+
+    public int[] grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new int[gridSize * gridSize];
+        for(int i = 0; i < grid.Length; i++)
+        {
+            grid[i] = Random.Range(0, 2);
+
+            var pos = new Vector3(-gridSize * 0.5f + i % gridSize, -gridSize * 0.5f * 0.75f + Mathf.Floor(i / gridSize) * 0.75f, 0f);
+            Instantiate(tile, pos, Quaternion.identity);
+
+            if (grid[i] == 1)
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
