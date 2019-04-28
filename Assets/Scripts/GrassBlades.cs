92d16b6772fec0f11f0c85077c828dec22ae112c
Antti Haavikko <antti.haavikko@focus.fi>
2019-04-28 18:03:38 +0300

    Try grass blades

---
+++ Assets/Scripts/GrassBlades.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassBlades : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var limit = 0.3f;
+        transform.localPosition = new Vector3(Random.Range(-limit, limit), Random.Range(-limit, limit), 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
