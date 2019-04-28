df3fd5ae241340865fe3a701fa2058769f2d28eb
Antti Haavikko <antti.haavikko@focus.fi>
2019-04-28 19:12:15 +0300

    Add icon and config dialog image

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,13 +4,13 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/hivemind:osx --fix-permissions
+butler push Builds/osx anttihaavikko/dmgot:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/hivemind:win --fix-permissions
+butler push Builds/win anttihaavikko/dmgot:win --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/hivemind:linux
+butler push Builds/linux anttihaavikko/dmgot:linux
 
 #echo "${COLOR}Pushing build for HTML5${NC}"
 #butler push Builds/html5 anttihaavikko/tic-tac-matrix:html5
