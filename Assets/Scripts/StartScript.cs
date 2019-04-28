f9c04a9ac879d7e264e0bcdbdba6ea4e899d7bf5
Antti Haavikko <antti.haavikko@focus.fi>
2019-04-28 17:42:54 +0300

    Add start scene

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    public Dimmer dimmer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CloseDimmer", 4.5f);
+        Invoke("DoStart", 5.5f);
+    }
+
+    void CloseDimmer()
+    {
+        dimmer.Close();
+    }
+
+    void DoStart()
+    {
+        SceneManager.LoadSceneAsync("Hut");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
